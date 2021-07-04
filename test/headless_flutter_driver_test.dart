e400597a210e760d23ba1bd06bd18f2114b04ab2
kaanyagci <kaan.yagci@clique.paris>
2021-07-04 20:04:41 +0200

    test(core): example test added
    test folder is automatically generated by dart create command

---
+++ test/headless_flutter_driver_test.dart
@@ -0,0 +1,8 @@
+import 'package:headless_flutter_driver/headless_flutter_driver.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
