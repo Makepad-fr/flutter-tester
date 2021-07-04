89796c4ff81bf654141a8e7623114c6070b08314
kaanyagci <kaan.yagci@clique.paris>
2021-07-04 20:40:52 +0200

    test(core): removed tests

--- test/headless_flutter_driver_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:headless_flutter_driver/headless_flutter_driver.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
