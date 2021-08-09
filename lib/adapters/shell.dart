e4c9d63dc05c33cf2fe2729ff83f4658f24d1c70
kaanyagci <kaan.yagci@clique.paris>
2021-08-10 00:04:45 +0200

    feat(adapters): Shell adapter updated
    When a shell command fails the stderr will be returned

--- lib/adapters/shell.dart
+++ lib/adapters/shell.dart
@@ -1,13 +1,10 @@
 import 'dart:io';
 
-import 'package:ft/models/runtime_exception.dart';
-
 String? run(String executable, List<String> arguments) {
   var process = Process.runSync(executable, arguments);
   if (process.exitCode == 0) {
     return process.stdout;
   } else {
-    throw RuntimeException(
-        executable: executable, arguments: arguments, stderr: process.stderr);
+    return process.stderr;
   }
 }
